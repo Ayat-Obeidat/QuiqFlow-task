d70e758af3f8798b0c34f47be1881c4a62226021
ghareeb <ayataliobeidat@gmail.com>
2025-04-15 20:48:52 +0300

    Making some improvements

--- problem-solving/The-Collatz-sequence-problem/task.js
+++ problem-solving/The-Collatz-sequence-problem/task.js
@@ -1,27 +1,26 @@
+function collatzLength(n, memo = {}) {
+  if (n in memo) return memo[n];
+  if (n === 1) return 1;
+
+  let next = n % 2 === 0 ? n / 2 : 3 * n + 1;
+  memo[n] = 1 + collatzLength(next, memo);
+  return memo[n];
+}
+
 function longestCollatz(limit) {
   let maxLength = 0;
-  let result = 0;
+  let startingNumber = 1;
+  let memo = {};
 
-  for (let i = 2; i < limit; i++) {
-    let n = i;
-    let length = 1;
-
-    while (n !== 1) {
-      if (n % 2 === 0) {
-        n = n / 2;
-      } else {
-        n = 3 * n + 1;
-      }
-      length++;
-    }
-
+  for (let i = 1; i < limit; i++) {
+    let length = collatzLength(i, memo);
     if (length > maxLength) {
       maxLength = length;
-      result = i;
+      startingNumber = i;
     }
   }
 
-  return result;
+  return startingNumber;
 }
 
 console.log(longestCollatz(1000000));
