d8a23c9ccb52deb41210f27d34d0ec461a964a73
ghareeb <ayataliobeidat@gmail.com>
2025-04-21 12:10:47 +0300

    the solution

--- problem-solving/Binary-Search-Array/index.js
+++ problem-solving/Binary-Search-Array/index.js
@@ -5,19 +5,22 @@
   Note that you can't just use indexOf. Your function must run in O(log(n)) time.
  */
 // the solution
-function binarySearch(arr, target) {
-  let left = 0;
-  let right = arr.length - 1;
 
-  while (left <= right) {
-    const mid = Math.floor((left + right) / 2);
+function findIndex(list, value) {
+  let start = 0;
+  let end = list.length - 1;
 
-    if (arr[mid] === target) {
-      return mid;
-    } else if (arr[mid] < target) {
-      left = mid + 1;
+  while (start <= end) {
+    const middle = Math.floor((start + end) / 2);
+
+    if (list[middle] === value) {
+      return middle;
+    }
+
+    if (list[middle] < value) {
+      start = middle + 1;
     } else {
-      right = mid - 1;
+      end = middle - 1;
     }
   }
 
